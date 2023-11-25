44df6311883c72a9b0930e546852df362f90916c
S.M Hasnan Monir <monirhasnan@gmail.com>
2023-11-25 18:42:20 +0600

    resolved

--- src/server.ts
+++ src/server.ts
@@ -7,9 +7,6 @@ const port = 5000
 
 async function main() {
   try {
-    await mongoose.connect(
-      `mongodb+srv://commonDB:3cBnSYImThHv7Wm8@cluster0.pyqmcvy.mongodb.net/?retryWrites=true&w=majority`,
-    )
     app.listen(port, () => {
       console.log(`Example app listening on port ${port}`)
     })
